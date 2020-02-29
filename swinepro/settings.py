ed8741151facc7692069399ad50eac8a7154885e
abhishekDutta666 <mailabhishekdutta@gmail.com>
2020-02-29 11:57:52 +0530

    refactoring of templates and static

--- swinepro/settings.py
+++ swinepro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z(cmbw793sk(v=(71@m^n!$j@)21n*gn089+nzh8#01gzrm+^a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,11 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+#MEDIA_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
 ]
 
 
